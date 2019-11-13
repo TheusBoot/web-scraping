e4fb7b4b023e48c1005f2634075138baaddb326b
Matheus Teixeira Trindade <Emailmartingcf1@gmail.com>
2019-11-13 18:39:14 -0200

    bs4

--- BeautifulSoup/Pesquisar_site.py
+++ BeautifulSoup/Pesquisar_site.py
@@ -1,1 +1,16 @@
-print('0')
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+
+class Page:
+    def __init__(self, driver):
+        """ Por Padrão, ele utiliza o Firefox, mas você poderia mudar de boa ;)"""
+        self.driver = driver
+
+
+Pesquisar = webdriver.Firefox()
+
+Pesquisar.get('http://www.google.com')
+
+print(Pesquisar.page_source)
+
+pagina = bs(Pesquisar,'')
